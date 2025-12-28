b122c28459a7b919efebf6b40a3a1dec70cef3ee
feldeq16 <felixdequidt@gmail.com>
2025-12-28 21:20:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide", page_title="Observatoire Climatique", page_ico
 st.title("🌍 Observatoire Climatique Multi-Scénarios")
 st.markdown("---")
 
-DOSSIER = "Donnéest"
+DOSSIER = "Données"
 FICHIER_DEFINITIONS = "name.txt"
 
 # ============================================
